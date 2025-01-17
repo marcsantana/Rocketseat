8650b8f396bb5dc2e0af78bd3d743eb094a8d9b9
Marcelo Santana <andersonmarcelo2002@gmail.com>
2025-01-17 12:48:27 -0300

    add remove-item-message container

--- lista-de-compras/script.js
+++ lista-de-compras/script.js
@@ -32,11 +32,47 @@ button_add.addEventListener("click", function(event) {
   button_li.appendChild(img);
 
   item.value = ""
+  
 
   // Adiciona o event listener ao botão de exclusão APÓS sua criação
   button_li.addEventListener("click", function() {
+
     const li = this.parentNode;
     ul.removeChild(li);
+
+    const div_remove_item_message = document.createElement("div")
+    div_remove_item_message.classList.add("flex")
+    div_remove_item_message.id = "remove-item-message"
+
+    const subdiv = document.createElement("div")
+    subdiv.classList.add("flex")
+
+    const img_exclamation = document.createElement("img")
+    img_exclamation.src = "assets/icons/exclamacao.svg";
+
+    const label = document.createElement("label")
+    label.textContent = "O item foi removido da lista"
+
+    subdiv.appendChild(img_exclamation)
+    subdiv.appendChild(label)
+
+    const img_close = document.createElement("img");
+    img_close.src = "assets/icons/x.svg";
+    
+    div_remove_item_message.appendChild(subdiv);
+    div_remove_item_message.appendChild(img_close);
+
+    document.body.appendChild(div_remove_item_message);
+
+    img_close.addEventListener("click", function() {
+      div_remove_item_message.parentNode.removeChild(div_remove_item_message); 
+    });
+
   });
 
 });
+
+
+
+// Chamar a função para adicionar a mensagem com a lógica da margem
+addRemoveMessage();
